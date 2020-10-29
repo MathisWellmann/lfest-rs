e10fdc986cfcdbd2dd14a0a77d197d74ab3e648e
MathisWellmann <wellmannmathis@gmail.com>
2020-10-29 13:00:14 +0000

    expose a clean public api

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,16 @@ pub mod config_float;
 
 pub mod contracts;
 mod welford_online;
+
+pub use exchange_float::MarginFloat;
+pub use exchange_float::PositionFloat;
+pub use exchange_float::ExchangeFloat;
+pub use exchange_decimal::ExchangeDecimal;
+pub use exchange_decimal::Margin as MarginDecimal;
+pub use exchange_decimal::Position as PositionDecimal;
+pub use contracts::ContractType;
+pub use config_float::Config as ConfigFloat;
+pub use config_decimal::Config as ConfigDecimal;
+pub use orders_decimal::Order as OrderDecimal;
+pub use orders_float::OrderFloat;
+
