7422656b592c8dad4cbb0760f83c50ff75290a86
MathisWellmann <wellmannmathis@gmail.com>
2023-05-10 18:28:25 +0000

    Re-enable the other three tests.

--- tests/inverse_futures.rs
+++ tests/inverse_futures.rs
@@ -522,78 +522,74 @@ fn inv_short_market_loss_partial() {
     if let Err(_) = pretty_env_logger::try_init() {}
 
     let mut exchange = mock_exchange_quote(base!(1));
-    todo!();
-    // let _ = exchange
-    //     .update_state(0, bba!(quote!(1000), quote!(1001)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(0, bba!(quote!(1000), quote!(1001)))
+        .unwrap();
 
-    // let value = base!(0.8);
-    // let size: QuoteCurrency = value.convert(exchange.bid());
-    // let o = Order::market(Side::Sell, size).unwrap();
-    // exchange.submit_order(o).unwrap();
+    let value = base!(0.8);
+    let size: QuoteCurrency = value.convert(exchange.market_state().bid());
+    let o = Order::market(Side::Sell, size).unwrap();
+    exchange.submit_order(o).unwrap();
 
-    // let _ = exchange
-    //     .update_state(1, bba!(quote!(999), quote!(1000)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(1, bba!(quote!(999), quote!(1000)))
+        .unwrap();
 
-    // let fee_quote1 = size.fee_portion(fee_taker);
-    // let fee_base1: BaseCurrency = fee_quote1.convert(quote!(1000));
+    let fee_quote1 = size.fee_portion(exchange.config().contract_specification().fee_taker);
+    let fee_base1: BaseCurrency = fee_quote1.convert(quote!(1000));
 
-    // assert_eq!(exchange.account().position().size(), size.into_negative());
-    // assert_eq!(exchange.account().position().entry_price(), quote!(1000.0));
-    // assert_eq!(
-    //     exchange
-    //         .account()
-    //         .position()
-    //         .unrealized_pnl(quote!(999), quote!(1000)),
-    //     base!(0.0)
-    // );
-    // assert_eq!(
-    //     exchange.account().margin().wallet_balance(),
-    //     base!(1.0) - fee_base1
-    // );
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.8));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     (base!(0.2) - fee_base1)
-    // );
+    assert_eq!(exchange.account().position().size(), size.into_negative());
+    assert_eq!(exchange.account().position().entry_price(), quote!(1000.0));
+    assert_eq!(
+        exchange
+            .account()
+            .position()
+            .unrealized_pnl(quote!(999), quote!(1000)),
+        base!(0.0)
+    );
+    assert_eq!(exchange.account().wallet_balance(), base!(1.0) - fee_base1);
+    assert_eq!(exchange.account().position().position_margin(), base!(0.8));
+    assert_eq!(
+        exchange.account().available_balance(),
+        (base!(0.2) - fee_base1)
+    );
 
-    // let _ = exchange
-    //     .update_state(1, bba!(quote!(1999), quote!(2000)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(1, bba!(quote!(1999), quote!(2000)))
+        .unwrap();
 
-    // let size = quote!(400.0);
-    // let fee_quote2 = size.fee_portion(fee_taker);
-    // let fee_base2: BaseCurrency = fee_quote2.convert(quote!(2000.0));
+    let size = quote!(400.0);
+    let fee_quote2 = size.fee_portion(exchange.config().contract_specification().fee_taker);
+    let fee_base2: BaseCurrency = fee_quote2.convert(quote!(2000.0));
 
-    // assert_eq!(
-    //     exchange
-    //         .account()
-    //         .position()
-    //         .unrealized_pnl(quote!(1999), quote!(2000)),
-    //     base!(-0.4)
-    // );
+    assert_eq!(
+        exchange
+            .account()
+            .position()
+            .unrealized_pnl(quote!(1999), quote!(2000)),
+        base!(-0.4)
+    );
 
-    // let o = Order::market(Side::Buy, size).unwrap();
-    // exchange.submit_order(o).unwrap();
+    let o = Order::market(Side::Buy, size).unwrap();
+    exchange.submit_order(o).unwrap();
 
-    // assert_eq!(exchange.account().position().size(), quote!(-400.0));
-    // assert_eq!(
-    //     exchange
-    //         .account()
-    //         .position()
-    //         .unrealized_pnl(quote!(1999), quote!(2000)),
-    //     base!(-0.2)
-    // );
-    // assert_eq!(
-    //     exchange.account().margin().wallet_balance(),
-    //     (base!(0.8) - fee_base1 - fee_base2)
-    // );
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.4));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     (base!(0.4) - fee_base1 - fee_base2)
-    // );
+    assert_eq!(exchange.account().position().size(), quote!(-400.0));
+    assert_eq!(
+        exchange
+            .account()
+            .position()
+            .unrealized_pnl(quote!(1999), quote!(2000)),
+        base!(-0.2)
+    );
+    assert_eq!(
+        exchange.account().wallet_balance(),
+        (base!(0.8) - fee_base1 - fee_base2)
+    );
+    assert_eq!(exchange.account().position().position_margin(), base!(0.4));
+    assert_eq!(
+        exchange.account().available_balance(),
+        (base!(0.4) - fee_base1 - fee_base2)
+    );
 }
 
 #[test]
@@ -601,77 +597,76 @@ fn inv_test_market_roundtrip() {
     if let Err(_) = pretty_env_logger::try_init() {}
 
     let mut exchange = mock_exchange_quote(base!(1));
-    todo!();
-    // let _ = exchange
-    //     .update_state(0, bba!(quote!(999), quote!(1000)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(0, bba!(quote!(999), quote!(1000)))
+        .unwrap();
 
-    // let value: BaseCurrency = exchange.account().margin().available_balance() * base!(0.9);
-    // let size = value.convert(exchange.ask());
-    // let buy_order = Order::market(Side::Buy, size).unwrap();
-    // exchange.submit_order(buy_order).unwrap();
-    // let _ = exchange
-    //     .update_state(1, bba!(quote!(1000), quote!(1001)))
-    //     .unwrap();
+    let value: BaseCurrency = exchange.account().available_balance() * base!(0.9);
+    let size = value.convert(exchange.market_state().ask());
+    let buy_order = Order::market(Side::Buy, size).unwrap();
+    exchange.submit_order(buy_order).unwrap();
+    let _ = exchange
+        .update_state(1, bba!(quote!(1000), quote!(1001)))
+        .unwrap();
 
-    // let sell_order = Order::market(Side::Sell, size).unwrap();
+    let sell_order = Order::market(Side::Sell, size).unwrap();
 
-    // exchange.submit_order(sell_order).unwrap();
+    exchange.submit_order(sell_order).unwrap();
 
-    // let fee_quote = size.fee_portion(fee_taker);
-    // let fee_base: BaseCurrency = fee_quote.convert(quote!(1000.0));
+    let fee_quote = size.fee_portion(exchange.config().contract_specification().fee_taker);
+    let fee_base: BaseCurrency = fee_quote.convert(quote!(1000.0));
 
-    // let _ = exchange
-    //     .update_state(2, bba!(quote!(1000.0), quote!(1001.0)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(2, bba!(quote!(1000.0), quote!(1001.0)))
+        .unwrap();
 
-    // assert_eq!(exchange.account().position().size(), quote!(0.0));
-    // assert_eq!(exchange.account().position().entry_price(), quote!(1000.0));
-    // assert_eq!(
-    //     exchange
-    //         .account()
-    //         .position()
-    //         .unrealized_pnl(quote!(1000), quote!(1001)),
-    //     base!(0.0)
-    // );
-    // assert_eq!(
-    //     exchange.account().margin().wallet_balance(),
-    //     base!(1.0) - base!(2.0) * fee_base
-    // );
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.0));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     base!(1.0) - base!(2.0) * fee_base
-    // );
+    assert_eq!(exchange.account().position().size(), quote!(0.0));
+    assert_eq!(exchange.account().position().entry_price(), quote!(1000.0));
+    assert_eq!(
+        exchange
+            .account()
+            .position()
+            .unrealized_pnl(quote!(1000), quote!(1001)),
+        base!(0.0)
+    );
+    assert_eq!(
+        exchange.account().wallet_balance(),
+        base!(1.0) - base!(2.0) * fee_base
+    );
+    assert_eq!(exchange.account().position().position_margin(), base!(0.0));
+    assert_eq!(
+        exchange.account().available_balance(),
+        base!(1.0) - base!(2.0) * fee_base
+    );
 
-    // let size = quote!(900.0);
-    // let buy_order = Order::market(Side::Buy, size).unwrap();
-    // exchange.submit_order(buy_order).unwrap();
-    // let _ = exchange
-    //     .update_state(3, bba!(quote!(1000.0), quote!(1001.0)))
-    //     .unwrap();
+    let size = quote!(900.0);
+    let buy_order = Order::market(Side::Buy, size).unwrap();
+    exchange.submit_order(buy_order).unwrap();
+    let _ = exchange
+        .update_state(3, bba!(quote!(1000.0), quote!(1001.0)))
+        .unwrap();
 
-    // let size = quote!(950.0);
-    // let sell_order = Order::market(Side::Sell, size).unwrap();
+    let size = quote!(950.0);
+    let sell_order = Order::market(Side::Sell, size).unwrap();
 
-    // exchange.submit_order(sell_order).unwrap();
+    exchange.submit_order(sell_order).unwrap();
 
-    // let _ = exchange
-    //     .update_state(4, bba!(quote!(998.0), quote!(1000.0)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(4, bba!(quote!(998.0), quote!(1000.0)))
+        .unwrap();
 
-    // assert_eq!(exchange.account().position().size(), quote!(-50.0));
-    // assert_eq!(exchange.account().position().entry_price(), quote!(1000.0));
-    // assert_eq!(
-    //     exchange
-    //         .account()
-    //         .position()
-    //         .unrealized_pnl(quote!(998), quote!(1000)),
-    //     base!(0.0)
-    // );
-    // assert!(exchange.account().margin().wallet_balance() < base!(1.0));
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.05));
-    // assert!(exchange.account().margin().available_balance() < base!(1.0));
+    assert_eq!(exchange.account().position().size(), quote!(-50.0));
+    assert_eq!(exchange.account().position().entry_price(), quote!(1000.0));
+    assert_eq!(
+        exchange
+            .account()
+            .position()
+            .unrealized_pnl(quote!(998), quote!(1000)),
+        base!(0.0)
+    );
+    assert!(exchange.account().wallet_balance() < base!(1.0));
+    assert_eq!(exchange.account().position().position_margin(), base!(0.05));
+    assert!(exchange.account().available_balance() < base!(1.0));
 }
 
 #[test]
@@ -679,70 +674,56 @@ fn inv_execute_limit() {
     if let Err(_) = pretty_env_logger::try_init() {}
 
     let mut exchange = mock_exchange_quote(base!(1));
-    todo!();
-    // let _ = exchange
-    //     .update_state(0, bba!(quote!(1000.0), quote!(1001.0)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(0, bba!(quote!(1000.0), quote!(1001.0)))
+        .unwrap();
 
-    // let o = Order::limit(Side::Buy, quote!(900.0), quote!(450.0)).unwrap();
-    // exchange.submit_order(o).unwrap();
-    // assert_eq!(exchange.account().active_limit_orders().len(), 1);
-    // assert_eq!(exchange.account().margin().wallet_balance(), base!(1.0));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     base!(0.49990)
-    // );
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.0));
-    // assert_eq!(exchange.account().margin().order_margin(), base!(0.5001)); // this includes the fee too
+    let o = Order::limit(Side::Buy, quote!(900.0), quote!(450.0)).unwrap();
+    exchange.submit_order(o).unwrap();
+    assert_eq!(exchange.account().active_limit_orders().len(), 1);
+    assert_eq!(exchange.account().wallet_balance(), base!(1.0));
+    assert_eq!(exchange.account().available_balance(), base!(0.49990));
+    assert_eq!(exchange.account().position().position_margin(), base!(0.0));
+    assert_eq!(exchange.account().order_margin(), base!(0.5001)); // this includes the fee too
 
-    // let (exec_orders, liq) = exchange
-    //     .update_state(1, bba!(quote!(750.0), quote!(751.0)))
-    //     .unwrap();
-    // assert!(!liq);
-    // assert_eq!(exec_orders.len(), 1);
+    let exec_orders = exchange
+        .update_state(1, bba!(quote!(750.0), quote!(751.0)))
+        .unwrap();
+    assert_eq!(exec_orders.len(), 1);
 
-    // assert_eq!(exchange.bid(), quote!(750.0));
-    // assert_eq!(exchange.ask(), quote!(751.0));
-    // assert_eq!(exchange.account().active_limit_orders().len(), 0);
-    // assert_eq!(exchange.account().position().size(), quote!(450.0));
-    // assert_eq!(exchange.account().position().entry_price(), quote!(900.0));
-    // assert_eq!(exchange.account().margin().wallet_balance(), base!(0.9999));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     base!(0.4999)
-    // );
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.5));
-    // assert_eq!(exchange.account().margin().order_margin(), base!(0.0));
+    assert_eq!(exchange.market_state().bid(), quote!(750.0));
+    assert_eq!(exchange.market_state().ask(), quote!(751.0));
+    assert_eq!(exchange.account().active_limit_orders().len(), 0);
+    assert_eq!(exchange.account().position().size(), quote!(450.0));
+    assert_eq!(exchange.account().position().entry_price(), quote!(900.0));
+    assert_eq!(exchange.account().wallet_balance(), base!(0.9999));
+    assert_eq!(exchange.account().available_balance(), base!(0.4999));
+    assert_eq!(exchange.account().position().position_margin(), base!(0.5));
+    assert_eq!(exchange.account().order_margin(), base!(0.0));
 
-    // let o = Order::limit(Side::Sell, quote!(1000.0), quote!(450.0)).unwrap();
-    // exchange.submit_order(o).unwrap();
-    // assert_eq!(exchange.account().active_limit_orders().len(), 1);
+    let o = Order::limit(Side::Sell, quote!(1000.0), quote!(450.0)).unwrap();
+    exchange.submit_order(o).unwrap();
+    assert_eq!(exchange.account().active_limit_orders().len(), 1);
 
-    // let _ = exchange
-    //     .update_state(1, bba!(quote!(1200.0), quote!(1201.0)))
-    //     .unwrap();
+    let _ = exchange
+        .update_state(1, bba!(quote!(1200.0), quote!(1201.0)))
+        .unwrap();
 
-    // assert_eq!(exchange.account().active_limit_orders().len(), 0);
-    // assert_eq!(exchange.account().position().size(), quote!(0.0));
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.0));
-    // assert_eq!(exchange.account().margin().wallet_balance(), base!(1.04981));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     base!(1.04981)
-    // );
+    assert_eq!(exchange.account().active_limit_orders().len(), 0);
+    assert_eq!(exchange.account().position().size(), quote!(0.0));
+    assert_eq!(exchange.account().position().position_margin(), base!(0.0));
+    assert_eq!(exchange.account().wallet_balance(), base!(1.04981));
+    assert_eq!(exchange.account().available_balance(), base!(1.04981));
 
-    // let o = Order::limit(Side::Sell, quote!(1200.0), quote!(600.0)).unwrap();
-    // exchange.submit_order(o).unwrap();
-    // assert_eq!(exchange.account().active_limit_orders().len(), 1);
+    let o = Order::limit(Side::Sell, quote!(1200.0), quote!(600.0)).unwrap();
+    exchange.submit_order(o).unwrap();
+    assert_eq!(exchange.account().active_limit_orders().len(), 1);
 
-    // let _ = exchange
-    //     .update_state(2, bba!(quote!(1200.1), quote!(1200.2)))
-    //     .unwrap();
-    // assert_eq!(exchange.account().position().size(), quote!(-600.0));
-    // assert_eq!(exchange.account().margin().position_margin(), base!(0.5));
-    // assert_eq!(exchange.account().margin().wallet_balance(), base!(1.04971));
-    // assert_eq!(
-    //     exchange.account().margin().available_balance(),
-    //     base!(0.54971)
-    // );
+    let _ = exchange
+        .update_state(2, bba!(quote!(1200.1), quote!(1200.2)))
+        .unwrap();
+    assert_eq!(exchange.account().position().size(), quote!(-600.0));
+    assert_eq!(exchange.account().position().position_margin(), base!(0.5));
+    assert_eq!(exchange.account().wallet_balance(), base!(1.04971));
+    assert_eq!(exchange.account().available_balance(), base!(0.54971));
 }
