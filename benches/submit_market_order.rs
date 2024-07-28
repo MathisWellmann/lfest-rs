b0a15c99aef5de507516d1219d7355bf22a8e3e8
MathisWellmann <wellmannmathis@gmail.com>
2024-07-28 22:30:42 +0200

    Add benchmark for `submit_market_order`

---
+++ benches/submit_market_order.rs
@@ -0,0 +1,60 @@
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use lfest::prelude::*;
+
+fn submit_market_orders<Q, U>(
+    exchange: &mut Exchange<
+        NoAccountTracker,
+        Q,
+        (),
+        InMemoryTransactionAccounting<Q::PairedCurrency>,
+    >,
+    order: &MarketOrder<Q, (), NewOrder>,
+    n: usize,
+) where
+    Q: Currency,
+    Q::PairedCurrency: MarginCurrency,
+{
+    for _ in 0..n {
+        exchange
+            .submit_market_order(order.clone())
+            .expect("Can submit market order");
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let starting_balance = base!(100000);
+    let acc_tracker = NoAccountTracker::default();
+    let contract_spec = ContractSpecification::new(
+        leverage!(1),
+        Dec!(0.5),
+        PriceFilter::new(None, None, quote!(0.5), Dec!(2), Dec!(0.5)).expect("is valid filter"),
+        QuantityFilter::new(None, None, quote!(0.1)).expect("is valid filter"),
+        fee!(0.0002),
+        fee!(0.0006),
+    )
+    .expect("works");
+    let config = Config::new(starting_balance, 200, contract_spec, 3600).unwrap();
+    let mut exchange = Exchange::new(acc_tracker, config);
+    exchange
+        .update_state(0.into(), &bba!(quote!(100), quote!(101)))
+        .expect("is valid market update");
+
+    let order = MarketOrder::new(Side::Buy, quote!(0.1)).unwrap();
+    let mut group = c.benchmark_group("submit_market_order");
+    const N: usize = 1_000;
+    group.throughput(criterion::Throughput::Elements(N as u64));
+    group.bench_function(&format!("submit_market_order_{N}"), |b| {
+        b.iter(|| {
+            submit_market_orders::<QuoteCurrency, Trade<QuoteCurrency>>(
+                black_box(&mut exchange),
+                black_box(&order),
+                N,
+            )
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
