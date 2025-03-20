e3b4b1e552b4bc6827cef513f6ff02dc557f64dd
MathisWellmann <wellmannmathis@gmail.com>
2025-03-20 15:08:32 +0100

    add `cargo-tarpaulin` to `flake.nix` and `mprocs.yml`. Got 62.27% coverage

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
               cargo-semver-checks
               cargo-mutants
               cargo-outdated
-              # cargo-tarpaulin # Code coverage
+              cargo-tarpaulin # Code coverage
               taplo
             ];
           };
