0a6aab6d638250b6a71302cd6d34725d51e2eacc
MathisWellmann <wellmannmathis@gmail.com>
2026-07-18 17:54:01 +0200

    add test to reproduce an accounting hole in position-reducing market order cases.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,7 @@
 mod amend;
 mod cancel_limit_order;
 mod partial_order_fill;
+mod reduce_position_order_margin;
 mod submit_limit_buy_order;
 mod submit_limit_sell_order;
 mod submit_market_buy_order;
